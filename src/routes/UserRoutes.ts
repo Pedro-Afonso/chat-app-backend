ca18cd0042c9193c8bc9ccb72d87d6d376eee65a
Pedro-Afonso <pedro.766@hotmail.com>
2022-11-12 18:30:09 -0300

    feat: Implement the login route

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,14 +1,18 @@
 import express from 'express'
 
 // Controllers
-import { register } from '../controllers/UserController'
+import { login, register } from '../controllers/UserController'
 
 // Middlewares
+import {
+  loginValidation,
+  userCreateValidation
+} from '../middlewares/userValidation'
 import { handleValidations } from '../middlewares/handleValidations'
-import { userCreateValidation } from '../middlewares/userValidation'
 
 const router = express()
 
+router.get('/login', loginValidation(), handleValidations, login)
 router.post('/register', userCreateValidation(), handleValidations, register)
 
 export { router as UserRoutes }
