ee30238ac87b8f12b171186f9e45c651ab8ac1d7
Pedro-Afonso <pedro.766@hotmail.com>
2022-11-12 18:26:22 -0300

    feat: Add loginValidation middleware

--- src/middlewares/userValidation.ts
+++ src/middlewares/userValidation.ts
@@ -29,4 +29,19 @@ const userCreateValidation = (): ValidationChain[] => {
   ]
 }
 
-export { userCreateValidation }
+const loginValidation = (): ValidationChain[] => {
+  return [
+    body('email')
+      .isString()
+      .withMessage('O e-mail é obrigatório.')
+      .isEmail()
+      .withMessage('Insira um e-mail válido.'),
+    body('password')
+      .isString()
+      .withMessage('O senha é obrigatória.')
+      .isLength({ min: 8 })
+      .withMessage('A senha precisa de no mínimo 8 caracteres.')
+  ]
+}
+
+export { userCreateValidation, loginValidation }
