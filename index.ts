e16fb65b0a04cf6de724dda18bf0261456ef386d
Pedro-Afonso <pedro.766@hotmail.com>
2022-11-12 16:18:51 -0300

    feat: Connect server to mongoDB

--- index.ts
+++ index.ts
@@ -1,8 +1,13 @@
 import express, { Request, Response } from 'express'
+import mongoose from 'mongoose'
 import cors from 'cors'
+import 'dotenv/config'
 
 const port = process.env.PORT || '5000'
 const app = express()
+
+// MongoDB uri
+const dbUri = process.env.DB_MONGO_URI
 
 // Allow JSON and form data
 app.use(express.json())
@@ -15,6 +20,16 @@ app.use(cors())
 app.get('/', (req: Request, res: Response) => {
   res.send(`API is working!`)
 })
+
+// Connect to mongoDB database
+if (dbUri) {
+  mongoose
+    .connect(dbUri)
+    // eslint-disable-next-line no-console
+    .then(() => console.log('Database connection successful!'))
+    // eslint-disable-next-line no-console
+    .catch(err => console.log(err))
+}
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
