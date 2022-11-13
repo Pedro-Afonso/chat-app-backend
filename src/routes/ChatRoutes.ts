f4edf63cbee659db5dc995d653d8f334eb5fdca5
Pedro-Afonso <pedro.766@hotmail.com>
2022-11-13 18:05:39 -0300

    feat: Implement the createGroupChat route

--- src/routes/ChatRoutes.ts
+++ src/routes/ChatRoutes.ts
@@ -2,13 +2,25 @@ import express from 'express'
 
 // Controllers
 import { accessChat, getAllChatsByUser } from '../controllers/ChatController'
+import { createGroupChat } from '../controllers/ChatController/createGroupChat'
 
 // Middlewares
 import { authGuard } from '../middlewares/authGuard'
+import { createGroupChatValidation } from '../middlewares/chatValidation'
+import { handleValidations } from '../middlewares/handleValidations'
 
 const router = express()
 
 router.post('/', authGuard, accessChat)
 router.get('/', authGuard, getAllChatsByUser)
+
+//  Create a new group chat
+router.post(
+  '/group',
+  authGuard,
+  createGroupChatValidation(),
+  handleValidations,
+  createGroupChat
+)
 
 export { router as ChatRoutes }
