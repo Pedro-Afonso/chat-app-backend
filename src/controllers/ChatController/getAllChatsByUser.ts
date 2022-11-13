21d138c002403cf4e37b41fad3e5534d4fb210ba
Pedro-Afonso <pedro.766@hotmail.com>
2022-11-13 14:34:24 -0300

    feat: Add getAllChatsByUser controller

---
+++ src/controllers/ChatController/getAllChatsByUser.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from 'express'
+import { ChatModel } from '../../models/ChatModel'
+
+/**
+  @description     Fetch all chats for a user
+  @route           GET /api/chats/
+  @access          Private
+ */
+export const getAllChatsByUser = async (req: Request, res: Response) => {
+  if (!req.user) {
+    res
+      .status(422)
+      .json({ errors: 'Houve um erro, por favor tente mais tarde.' })
+    return
+  }
+
+  const filterOptions = { users: { $in: req.user._id } }
+
+  const populateOptions = [
+    { path: 'users', select: '-password' },
+    { path: 'groupAdmin', select: '-password' },
+    {
+      path: 'latestMessage',
+      populate: { path: 'sender', select: 'name profileImage email' }
+    }
+  ]
+
+  const chat = await ChatModel.find(filterOptions)
+    .populate(populateOptions)
+    .sort({ updatedAt: 'desc' })
+
+  res.status(200).json(chat)
+}
--- src/controllers/ChatController/index.ts
+++ src/controllers/ChatController/index.ts
@@ -1,1 +1,2 @@
 export * from './accessChat'
+export * from './getAllChatsByUser'
