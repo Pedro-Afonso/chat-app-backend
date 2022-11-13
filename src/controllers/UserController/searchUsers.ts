bb56209925edcc1954d6e2bcf21d01681e792a87
Pedro-Afonso <pedro.766@hotmail.com>
2022-11-12 21:53:24 -0300

    feat: Add  search users

---
+++ src/controllers/UserController/searchUsers.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from 'express'
+import { UserModel } from '../../models/UserModel'
+
+/**
+ * @description     Get or Search all users
+ * @route           GET /api/users/search?q=
+ * @access          Private
+ */
+
+export const searchUsers = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const keyword = req.query.q
+    ? {
+        $or: [
+          { name: { $regex: req.query.q, $options: 'i' } },
+          { email: { $regex: req.query.q, $options: 'i' } }
+        ]
+      }
+    : {}
+
+  if (!req.user) {
+    res
+      .status(422)
+      .json({ errors: ['Houve um erro, por favor tente mais tarde.'] })
+    return
+  }
+
+  const users = await UserModel.find(keyword).find({
+    _id: { $ne: req.user._id }
+  })
+  res.status(200).json(users)
+}
--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 
 // Controllers
-import { login, register } from '../controllers/UserController'
+import { login, register, searchUsers } from '../controllers/UserController'
 
 // Middlewares
 import {
@@ -9,10 +9,12 @@ import {
   userCreateValidation
 } from '../middlewares/userValidation'
 import { handleValidations } from '../middlewares/handleValidations'
+import { authGuard } from '../middlewares/authGuard'
 
 const router = express()
 
 router.get('/login', loginValidation(), handleValidations, login)
+router.get('/search', authGuard, searchUsers)
 router.post('/register', userCreateValidation(), handleValidations, register)
 
 export { router as UserRoutes }
