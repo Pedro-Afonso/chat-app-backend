3b84911eee0416c6f7654e379421ef752533baeb
Pedro-Afonso <pedro.766@hotmail.com>
2022-11-15 11:57:23 -0300

    feat: Add getCurrentUser controller

---
+++ src/controllers/UserController/getCurrentUser.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express'
+import 'dotenv/config'
+
+import { UserModel } from '../../models/UserModel'
+
+/**
+  @description     Get current user profile
+  @route           GET /api/users/
+  @access          Public
+ */
+export const getCurrentUser = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const user =
+    req.user && (await UserModel.findById(req.user._id).select('-password'))
+
+  // Check if user exists
+  if (!user) {
+    res.status(404).json({ errors: ['Usuário não encontrado'] })
+    return
+  }
+
+  // Return user profile
+  res.status(200).json(user)
+}
--- src/controllers/UserController/index.ts
+++ src/controllers/UserController/index.ts
@@ -1,3 +1,4 @@
 export * from './register'
 export * from './login'
 export * from './searchUsers'
+export * from './getCurrentUser'
