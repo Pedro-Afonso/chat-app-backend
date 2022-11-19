81a0b6608c603984fd0885a6f7fe76d82bc83d9b
Pedro-Afonso <pedro.766@hotmail.com>
2022-11-19 18:28:52 -0300

    feat: Update route

--- src/routes/MessageRoutes.ts
+++ src/routes/MessageRoutes.ts
@@ -12,6 +12,6 @@ import { authGuard } from '../middlewares/authGuard'
 const router = express()
 
 router.post('/', authGuard, sendMessage)
-router.get('/:chatId', authGuard, getAllMessagesByChat)
+router.get('/chat/:chatId', authGuard, getAllMessagesByChat)
 
 export { router as MessageRoutes }
